03514a22f7d36cfd8d21388a98eba26ae412b530
GLeoni17 <gabriel.lduarte@outlook.com>
2020-03-03 17:49:19 -0300

    Versao 5 - Adicionado index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h1>Pagina com links dos meus exemplos.</h1>
+        <a href="20200303/exemplo1.html">Exemplo 1</a>
+        <br>
+        <a href="20200303/exemplo2.html">Exemplo 2</a>
+        <br>
+        <a href="20200303/exemplo3.html">Exemplo 3</a>
+        <br>
+        <a href="20200303/exemplo3a.html">Exemplo 3a</a>
+        <br>
+	</body>
+</html>
