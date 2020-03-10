e388dfb412209415dd5f3ce51723f39033ee757b
GLeoni17 <gabriel.lduarte@outlook.com>
2020-03-10 17:51:11 -0300

    Versao 2 - Editado index.php e adicionados exemplos do dia 10/03/2020

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	</head>
 	<body>
 		<h1>Pagina com links dos meus exemplos.</h1>
+		<h3>03/03/2020</h3>
         <a href="20200303/exemplo1.html">Exemplo 1</a>
         <br>
         <a href="20200303/exemplo2.html">Exemplo 2</a>
@@ -13,5 +14,20 @@
         <br>
         <a href="20200303/exemplo3a.html">Exemplo 3a</a>
         <br>
+		<h3>10/03/2020</h3>
+		<a href="20200310/ex_onchange.html">Exemplo Onchange</a>
+		<br>
+		<a href="20200310/ex_onclick.html">Exemplo Onclick</a>
+		<br>
+		<a href="20200310/ex_onblur.html">Exemplo Onblur</a>
+		<br>
+		<a href="20200310/ex_onfocus.html">Exemplo Onfocus</a>
+		<br>
+		<a href="20200310/ex_onmouseover_onmouseout.html">Exemplo Onmouseover e Onmouseout</a>
+		<br>
+		<a href="20200310/ex_onkeydown_onkeyup.html">Exemplo Onkeydown e Onkeyup</a>
+		<br>
+		<a href="20200310/ex_onload.html">Exemplo Onload</a> (Cuidado para executar, tem +1.000.000 de linhas)
+		<br>
 	</body>
 </html>
